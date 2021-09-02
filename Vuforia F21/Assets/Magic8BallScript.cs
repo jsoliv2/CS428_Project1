6e8264bf2c992c2eacb80a75f133c9a6118ecefe
jsoliv2 <57681366+jsoliv2@users.noreply.github.com>
2021-09-02 15:51:35 -0500

    Added Weather API and created Magic 8 Ball Script

---
+++ Vuforia F21/Assets/Magic8BallScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Magic8BallScript : MonoBehaviour
+{
+    public string[] phrases;
+    public GameObject knickknack;
+    public TMP_Text magic8Text;
+
+    public float rotationTolerance = 30.0f;
+
+    private bool isUpsideDown = false;
+    private bool isUpsideDownPrev = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUpsideDown();
+
+        CheckTurnRightsideUp();
+
+        isUpsideDownPrev = isUpsideDown;
+    }
+
+    void CheckUpsideDown()
+    {
+        float baseRotation = 180.0f;
+        float currRotation = knickknack.transform.eulerAngles.x;
+        Debug.Log(knickknack.transform.eulerAngles);
+
+        if ((currRotation >= (baseRotation - rotationTolerance) && (currRotation <= (baseRotation + rotationTolerance)))
+         || (currRotation >= (-baseRotation - rotationTolerance) && (currRotation <= (-baseRotation + rotationTolerance))))
+        {
+            isUpsideDown = true;
+        }
+        else
+        {
+            isUpsideDown = false;
+        }
+    }
+
+    void CheckTurnRightsideUp()
+    {
+        if (!isUpsideDown && isUpsideDownPrev)
+        {
+            if (phrases.Length > 0)
+            {
+                magic8Text.text = phrases[Random.Range(0, phrases.Length)];
+                Debug.Log("RSU");
+            }
+        }
+    }
+}
--- Vuforia F21/Assets/WeatherAPIScript.cs
+++ Vuforia F21/Assets/WeatherAPIScript.cs
@@ -7,7 +7,7 @@ public class WeatherAPIScript : MonoBehaviour
 {
     public GameObject weatherTextObject;
         // add your personal API key after APPID= and before &units=
-       string url = "http://api.openweathermap.org/data/2.5/weather?lat=33.39&lon=-104.5&APPID=<>&units=imperial";
+       string url = "http://api.openweathermap.org/data/2.5/weather?lat=33.39&lon=-104.5&APPID=ad402628759d076c74b1e1ea1e64ba9b&units=imperial";
 
    
     void Start()
